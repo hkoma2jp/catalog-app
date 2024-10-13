9ebc64fe36194c93d642faf314df28058b14bbb3
hkoma2jp <h.koma2.jp@gmail.com>
2024-10-13 23:48:30 +0900

    add entity classes
    - add entity/Product.java
    - add entity/Sku.java
    - add entity/Spec.java
    - add entity/Category.java

---
+++ graphql/src/main/java/com/hkoma2jp/graphql/entity/Spec.java
@@ -0,0 +1,40 @@
+package com.hkoma2jp.graphql.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+
+
+@Entity
+@Data
+@Table(name = "specs")
+public class Spec {
+
+    @Id
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "sku_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Sku sku;
+
+    @Column(nullable = false)
+    private String specName;
+
+    @Column(nullable = false)
+    private String specValue;
+
+    @Column(nullable = false)
+    private int sortOrder;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+}
